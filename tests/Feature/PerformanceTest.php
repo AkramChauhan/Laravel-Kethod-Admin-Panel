164de421a6d8a5280f2e12b8f7f234cf9d0a2a40
Akram <akram.aic193@gmail.com>
2024-05-13 05:59:52 +0530

    Performance Test for the Login PagE

--- tests/Feature/PerformanceTest.php
+++ tests/Feature/PerformanceTest.php
@@ -25,8 +25,31 @@ class PerformanceTest extends TestCase {
 
     // Calculate execution time
     $executionTime = $endTime - $startTime;
-    
+
     // Assert that the execution time is within an acceptable range
-    $this->assertLessThan(1000, $executionTime); 
+    $this->assertLessThan(1000, $executionTime);
+  }
+
+  /**
+   * Test the performance of a specific endpoint.
+   */
+  public function testShouldLoadLoginPageWithinOneSecond() {
+    // Start measuring time
+    $startTime = microtime(true);
+
+    // Make HTTP request to the endpoint you want to test
+    $response = $this->get('/login');
+
+    // Check if the response is successful (status code 200)
+    $response->assertStatus(200);
+
+    // End measuring time
+    $endTime = microtime(true);
+
+    // Calculate execution time
+    $executionTime = $endTime - $startTime;
+
+    // Assert that the execution time is within an acceptable range
+    $this->assertLessThan(1000, $executionTime);
   }
 }
