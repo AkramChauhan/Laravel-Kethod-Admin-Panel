12e424e8a0e281ad72873bffecff57f099a7a51e
Akram <akram.aic193@gmail.com>
2024-05-13 05:58:41 +0530

    Performance Test for the Welcome Page

---
+++ tests/Feature/PerformanceTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PerformanceTest extends TestCase {
+  /**
+   * Test the performance of a specific endpoint.
+   */
+  public function testShouldLoadWelcomePageWithinOneSecond() {
+    // Start measuring time
+    $startTime = microtime(true);
+
+    // Make HTTP request to the endpoint you want to test
+    $response = $this->get('/');
+
+    // Check if the response is successful (status code 200)
+    $response->assertStatus(200);
+
+    // End measuring time
+    $endTime = microtime(true);
+
+    // Calculate execution time
+    $executionTime = $endTime - $startTime;
+    
+    // Assert that the execution time is within an acceptable range
+    $this->assertLessThan(1000, $executionTime); 
+  }
+}
